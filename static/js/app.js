6176ae01fe7876dc5bb58e89131633275df1f2c3
Tsmoore002900 <tgem9914@gmail.com>
2023-04-12 19:51:59 -0400

    add files

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+// Use the D3 library to read in samples.json from the URL 
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+// Create a bubble chart that displays each sample.
+
+// Display the sample metadata, i.e., an individual's demographic information.
+
+
+// Update all the plots when a new sample is selected. Additionally, you are welcome to create any layout that you would like for your dashboard.
+
+// Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo.
