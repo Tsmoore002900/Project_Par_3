e9b6462d6f0c1673f61f1b95afc988b626adebe0
Tsmoore002900 <tgem9914@gmail.com>
2023-04-16 11:22:40 -0400

    add test.html for dropdown menus

---
+++ static/js/dropdown.js
@@ -0,0 +1,11 @@
+// Display the sample metadata, i.e., an golfers information.
+const url = "https://raw.githubusercontent.com/Tsmoore002900/Project_Par_3/main/merged_golf_data.json"
+
+d3.json(url).then(function(data) {
+    console.log(data);
+});
+
+function init() {
+    let dropDownMenu = d3.select("#selDataset");
+    let dataset = dropdownMenu.property("value");
+};
