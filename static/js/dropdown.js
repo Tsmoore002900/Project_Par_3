8eaae638001a22bf769ac84a6cfba255385d2e95
Tsmoore002900 <tgem9914@gmail.com>
2023-04-16 11:26:52 -0400

    remove test data js file

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,5 +1,6 @@
 // Display the sample metadata, i.e., an golfers information.
-const url = "https://raw.githubusercontent.com/Tsmoore002900/Project_Par_3/main/merged_golf_data.json"
+// const url = "https://raw.githubusercontent.com/Tsmoore002900/Project_Par_3/main/merged_golf_data.json"
+const url = "https://raw.githubusercontent.com/Tsmoore002900/Project_Par_3/main/test_golf_data.json"
 
 d3.json(url).then(function(data) {
     console.log(data);
